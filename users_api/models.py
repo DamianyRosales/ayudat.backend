881a1d0de8e2f6f5a019daf0428b284b9db1ae8e
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-04-05 09:05:36 +0000

    git testing 2

--- users_api/models.py
+++ users_api/models.py
@@ -44,7 +44,7 @@ class Mod(UserBase):
 
 
 class Professional(UserBase):
-    #
+    
     # CURP field 18 characters as max length
     curp_validator = RegexValidator(regex=r'^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$', message="")
     curp = models.CharField(max_length=18, unique=True,validators=[curp_validator], blank=True, null=True)
