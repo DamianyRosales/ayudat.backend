553a1c826be98a60336cfc2759fc2350e49209d0
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-04-07 23:34:19 -0500

    URL create user fixed

--- users_api/urls.py
+++ users_api/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('professional/create/', views.professional_view_post.as_view()),
 
     path('patient/', views.patient_view.as_view()),
-    path('patient/create', views.patient_view_post.as_view()),
+    path('patient/create/', views.patient_view_post.as_view()),
     
     path('mod/', views.mod_view.as_view()),
-    path('mod/create', views.mod_view_post.as_view()),
+    path('mod/create/', views.mod_view_post.as_view()),
     
     path('admin/', views.admin_view.as_view()),
-    path('admin/create', views.admin_view_post.as_view()),
+    path('admin/create/', views.admin_view_post.as_view()),
     # path('professional/<str:email>', views.professional_view.as_view()),
 ]
