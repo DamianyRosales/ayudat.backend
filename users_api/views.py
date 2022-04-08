b9ee02937a1f3ad5b6f3d8e3ce771583a9e9dfed
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-04-08 14:54:23 -0500

    Token fixed 2

--- users_api/views.py
+++ users_api/views.py
@@ -23,8 +23,104 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from rest_framework import permissions
 
+from .models import Professional, Admin, Mod, UserBase, Patient
+
+from django.utils.module_loading import import_string
+
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.authentication import AUTH_HEADER_TYPES
+
 # Create your views here.
 
+from rest_framework import generics, status
+
+class TokenViewBase(generics.GenericAPIView):
+    permission_classes = ()
+    authentication_classes = ()
+
+    serializer_class = None
+    _serializer_class = ""
+
+    www_authenticate_realm = "api"
+
+    def get_serializer_class(self):
+        """
+        If serializer_class is set, use it directly. Otherwise get the class from settings.
+        """
+
+        if self.serializer_class:
+            return self.serializer_class
+        try:
+            return import_string(self._serializer_class)
+        except ImportError:
+            msg = "Could not import serializer '%s'" % self._serializer_class
+            raise ImportError(msg)
+
+    def get_authenticate_header(self, request):
+        return '{} realm="{}"'.format(
+            AUTH_HEADER_TYPES[0],
+            self.www_authenticate_realm,
+        )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        email = request.data.get('email')
+        uemail = 'email'
+        userType=-1
+        for e in Patient.objects.all():
+            if e.email == email:
+                userType = e.userType
+                uemail = e.email
+
+        for e in Admin.objects.all():
+            if e.email == email:
+                userType = e.userType
+                uemail = e.email
+        for e in Mod.objects.all():
+            if e.email == email:
+                userType = e.userType
+                uemail = e.email
+        for e in Professional.objects.all():
+            if e.email == email:
+                professional = Professional.objects.get(email=e.email)
+                
+                serializer2 = ProfessionalSerializer(professional)
+                userType = e.userType
+                uemail = e.email
+                document1 = serializer2.data['document1']
+                document2 = serializer2.data['document2']
+                
+                try:
+                    serializer.is_valid(raise_exception=True)
+                except TokenError as e:
+                    raise InvalidToken(e.args[0])
+
+                return JsonResponse(data={
+                    'data':serializer.validated_data, 
+                    'userType':userType,
+                    'email':uemail, 
+                    'document1':document1, 
+                    'document2':document2
+                    }, 
+                    status=status.HTTP_200_OK)
+
+        
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        return JsonResponse(data={'data':serializer.validated_data, 'userType':userType, 'email':uemail}, status=status.HTTP_200_OK)
+
+class TokenObtainPairView(TokenViewBase):
+    """
+    Takes a set of user credentials and returns an access and refresh JSON web
+    token pair to prove the authentication of those credentials.
+    """
+
+    _serializer_class = api_settings.TOKEN_OBTAIN_SERIALIZER
 
 class EmailTokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
